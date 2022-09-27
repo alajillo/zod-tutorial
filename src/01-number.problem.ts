76262f77911b22c641ecf50e41832f4eb7acbbb7
alajillo <alajillo2021@gmail.com>
2022-09-27 19:56:17 +0900

    Solove : 01 ~ 05

--- src/01-number.problem.ts
+++ src/01-number.problem.ts
@@ -3,9 +3,10 @@
 import { expect, it } from "vitest";
 import { z } from "zod";
 //       ^ 🕵️‍♂️
-
+const numberSchema = z.number()
 export const toString = (num: unknown) => {
-  return String(num);
+  const parsed = numberSchema.parse(num);
+  return String(parsed);
 };
 
 // TESTS
